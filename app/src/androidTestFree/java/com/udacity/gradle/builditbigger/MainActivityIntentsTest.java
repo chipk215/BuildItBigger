3bc18d38f313ef214dee8936e2078747885d058b
Chip Keyes <chipk215@msn.com>
2018-03-25 11:03:23 -0600

    More free main activity tests

---
+++ app/src/androidTestFree/java/com/udacity/gradle/builditbigger/MainActivityIntentsTest.java
@@ -0,0 +1,41 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.keyeswest.jokeviewer.JokeViewerMainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.BundleMatchers.hasEntry;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtras;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityIntentsTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void verifySentIntentTest() {
+        //click the joke button
+        onView(withId(R.id.joke_btn)).perform(click());
+
+        intended(toPackage("com.udacity.gradle.builditbigger.free"));
+
+        intended(hasExtras(hasEntry(equalTo(JokeViewerMainActivity.JOKE_KEY), instanceOf(String.class))));
+
+    }
+}
--- app/src/androidTestFree/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTestFree/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,20 +1,25 @@
 package com.udacity.gradle.builditbigger;
 
 
+
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -23,6 +28,7 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule =
             new ActivityTestRule<>(MainActivity.class, false, false);
+
 
 
     @Test
@@ -46,4 +52,18 @@ public class MainActivityTest {
         onView(withId(R.id.adView)).check(matches(isDisplayed()));
 
     }
+
+    @Test
+    public void clickJokeButtonTest(){
+        mActivityTestRule.launchActivity(null);
+
+        //click the joke button
+        onView(withId(R.id.joke_btn)).perform(click());
+
+        // verify the library provided view is displayed
+        onView(withId(com.keyeswest.jokeviewer.R.id.joke_tv)).check(matches(isDisplayed()));
+    }
+
+
+
 }
